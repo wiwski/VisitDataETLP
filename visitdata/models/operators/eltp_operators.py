410e2bcd977cb65b7b3464d220626b6963059a0f
Witold Wroblewski <witoldwrob@gmail.com>
2019-12-04 15:42:57 +0100

    Continue implementation of ELTPOperator

--- visitdata/models/operators/eltp_operators.py
+++ visitdata/models/operators/eltp_operators.py
@@ -2,4 +2,21 @@ class ELTPOperator:
     """
     Abstract class for all operators following the Extract Load Transform PostProcess pattern.
     """
-    pass
+
+    datasource_id = None
+    
+    def init(self, datahub_task_id):
+        # TODO implement init logic
+        self.fetch_datasource(datahub_task_id)
+        pass
+
+    def execute(self, context):
+        # TODO set datasource_id after fetching
+        self.datasource_id = None
+        pass
+
+    def fetch_datasource(self, datahub_task_id):
+        """
+        Fetch Datasource information
+        """
+        pass
