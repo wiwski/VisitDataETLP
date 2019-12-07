ed7b87ac83868d2834eddc6b65760d7b4a49ffd3
Witold <witoldwrob@gmail.com>
2019-12-07 16:53:25 +0100

    Add Visit Data Redshift connection

--- visitdata/settings.py
+++ visitdata/settings.py
@@ -6,22 +6,41 @@ from dotenv import load_dotenv
 from airflow import settings
 from airflow.models import Connection
 
-from .models.hooks import VDS3Hook
+from visitdata.models.hooks.vd_rs_hook import VDRSHook
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Setting up Visit Data S3 Connection
-VD_S3_CON_ID = "VD_S3"
-VD_S3_CONNECTION = Connection(
-    conn_id="VD_S3",
-    conn_type="s3",
-    login=os.getenv('VD_S3_ACCESS_KEY'),
-    password=os.getenv('VD_S3_SECRET_KEY'),
-    extra=json.dumps({"default_bucket": os.getenv('VD_S3_DEFAULT_BUCKET')}))
 session = settings.Session()
-# Removing old connections as Airflow save
-session.query(Connection).filter(Connection.conn_id == VD_S3_CON_ID).delete()
-session.add(VD_S3_CONNECTION)
-session.commit()
+
+# Setting up Visit Data S3 and Redshift Connections
+CONNECTIONS = [
+    Connection(
+        conn_id="VD_S3",
+        conn_type="s3",
+        login=os.getenv('VD_S3_ACCESS_KEY'),
+        password=os.getenv('VD_S3_SECRET_KEY'),
+        extra=json.dumps({"default_bucket": os.getenv('VD_S3_DEFAULT_BUCKET')})
+    ),
+
+    Connection(
+        conn_id="VD_RS",
+        conn_type="redshift",
+        host=os.getenv('VD_RS_HOST'),
+        login=os.getenv('VD_RS_USERNAME'),
+        password=os.getenv('VD_RS_PASSWORD'),
+        schema=os.getenv('VD_RS_DATABASE'),
+        port=os.getenv('VD_RS_PORT')
+    )
+]
+
+for conn in CONNECTIONS:
+    # Removing old connections as Airflow would
+    # save multiple connections with same ID
+    session.query(Connection).filter(
+        Connection.conn_id == conn.conn_id).delete()
+    # Add / Re-add the connection
+    session.add(conn)
+    session.commit()
+
 session.close()
